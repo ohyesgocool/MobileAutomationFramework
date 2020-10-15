d44cbd69740052dcd006055b6a9e33d20900ea75
Gokul Menon <ohmygocool@yahoo.com>
2020-10-15 13:10:04 +0530

    initial commit- base template

---
+++ MobilAutomationFramework/src/test/java/pages/HomePage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+
+public class HomePage extends BaseTest {
+
+	public static void main(String[] args) throws MalformedURLException, Throwable {
+		capability();
+		
+
+		
+		
+
+	}
+}
